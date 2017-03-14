b5dee0abb421ea977d62444dcd0e7169b225c24a
Marcin <spock@omegastar.eu>
2017-03-14 22:09:01 +0100

    Author fix, since I'm author.

--- src/sensors/bmp180_barometer.rs
+++ src/sensors/bmp180_barometer.rs
@@ -1,10 +1,13 @@
-// Copyright 2015, Paul Osborne <osbpau@gmail.com>
+// Copyright 2017, Marcin Jurczuk <marcin@jurczuk.eu>
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/license/LICENSE-2.0> or the MIT license
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option.  This file may not be copied, modified, or distributed
 // except according to those terms.
+
+// This module is implementing userland library to interact with
+// Bosh BMP180 i2c temperature and pressure sensor.
 
 #![allow(dead_code)]
 
